483ea5843b9a0cf877d8c514b393fe2f97ff193e
mglinka <marta.glinka2002@gmail.com>
2025-03-23 23:33:34 +0100

    oświadczenia fix

--- src/app/ScannedProduct.tsx
+++ src/app/ScannedProduct.tsx
@@ -109,13 +109,14 @@ const ScannedProduct: React.FC<ScannedProductProps> = ({ scanned }) => {
           </section>
           <section>
             <div className="p-4 border-2 border-gray-100 rounded-lg shadow-md flex flex-col gap-4">
-              <div className="font-light text-xl flex items-center space-x-4 pl-6">
-                <ul className="list-disc  space-y-2">
+              <div className="font-light text-xl flex items-center space-x-4">
+                <ul className="list-disc  space-y-2 ">
                   {ProductInfo.statements.split(", ").map((item, index) => (
-                      <li key={index} className="font-light text-xl flex items-center space-x-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                      <li key={index} className="font-light text-xl flex items-center space-x-4 ">
+                        <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                           <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
                         </svg>
+                        </div>
                         <span>{item}</span>
                       </li>
                   ))}
@@ -126,7 +127,7 @@ const ScannedProduct: React.FC<ScannedProductProps> = ({ scanned }) => {
 
           <section>
             <div className="p-4 border-2 border-gray-100 rounded-lg shadow-md flex flex-col gap-4">
-              <div className="font-light text-xl flex items-center space-x-4 pl-6">
+              <div className="font-light text-xl flex items-center space-x-4">
               <ul className="list-disc  space-y-2">
                 {ProductInfo.parameters.split(", ").map((item, index) => (
                     <li key={index} className="font-light text-xl flex items-center space-x-4">

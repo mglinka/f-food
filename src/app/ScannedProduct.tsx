713b8a7c161408cfa00a47295dc26bc174e256f0
mglinka <marta.glinka2002@gmail.com>
2025-03-23 22:59:56 +0100

    dodany drop down w opisie

--- src/app/ScannedProduct.tsx
+++ src/app/ScannedProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ENScoreIndicator from "@/components/indexes/ENScoreIndicator";
 import ScoreIndicator from "@/components/indexes/ScoreIndicator";
 
@@ -10,9 +10,16 @@ interface ScannedProductProps {
 const ProductInfo = {
   productName:"Product 1",
   productDescription:"Product 1 description",
+  EAN:"123456789111",
+  country:"Poland",
+  package:"Package 1",
+  composition: "mleko, sól, E509, E330",
+  parameters: "VEGE, bez glutenu, bez laktozy, bez GMO"
 }
 
 const ScannedProduct: React.FC<ScannedProductProps> = ({ scanned }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
       <div className="grid grid-cols-1 md:grid-cols-9 gap-6 p-4">
         <div className="space-y-4 md:col-span-4">
@@ -26,9 +33,47 @@ const ScannedProduct: React.FC<ScannedProductProps> = ({ scanned }) => {
           </section>
           <section>
             <div className="p-4 rounded-lg shadow-md border-2 border-gray-100">
-
               <h1 className="text-2xl font-medium text-black mb-2">Opis</h1>
-              <p className="font-light text-md"><span>Mleko spożywcze pasteryzowane w wysokiej temperaturze. Zawartość tłuszczu 3,2%</span></p>
+              <p className="font-light text-md">
+                Mleko spożywcze pasteryzowane w wysokiej temperaturze. Zawartość tłuszczu 3,2%
+              </p>
+
+              {isExpanded && (
+                  <div className="mt-2 text-gray-700">
+                    <p className="flex items-center space-x-2">
+                      <span>Kraj</span>
+                      <img src={"/right-arrow.png"} alt="Arrow" className="w-4 h-4 object-contain" />
+                      <span>{ProductInfo.country}</span>
+                    </p>
+                    <p className="flex items-center space-x-2">
+                      <span>Opakowanie</span>
+                      <img src={"/right-arrow.png"} alt="Arrow" className="w-4 h-4 object-contain" />
+                      <span>{ProductInfo.package}</span>
+                    </p>
+                    <p className="flex items-center space-x-2">
+                      <span>Kod EAN</span>
+                      <img src={"/right-arrow.png"} alt="Arrow" className="w-4 h-4 object-contain" />
+                      <span>{ProductInfo.EAN}</span>
+                    </p>
+                  </div>
+              )}
+
+              <button
+                  onClick={() => setIsExpanded(!isExpanded)}
+                  className="flex items-center justify-between w-full mt-4 text-white font-medium"
+              >
+                <span>{isExpanded ? "Zwiń" : "Czytaj więcej"}</span>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    className={`transform transition-transform duration-300 ${
+                        isExpanded ? "rotate-180" : "rotate-0"
+                    }`}
+                >
+                  <path d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"/>
+                </svg>
+              </button>
             </div>
           </section>
 
@@ -41,7 +86,7 @@ const ScannedProduct: React.FC<ScannedProductProps> = ({ scanned }) => {
                   <span className="text-base sm:text-xl font-light">Indeks EN</span>
                 </div>
                 <div>
-                  <ENScoreIndicator highlightedIndex={3} />
+                  <ENScoreIndicator highlightedIndex={1} />
                 </div>
 
                 <div className="flex items-center space-x-4 px-4 sm:px-6">
@@ -65,44 +110,35 @@ const ScannedProduct: React.FC<ScannedProductProps> = ({ scanned }) => {
           <section>
             <div className="p-4 border-2 border-gray-100 rounded-lg shadow-md flex flex-col gap-4">
               <div className="font-light text-xl flex items-center space-x-4 pl-6">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                  <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
-                </svg>
-
-                <span className={""}>Bez gutenu</span>
-              </div>
-              <div className="font-light text-xl flex items-center space-x-4 pl-6">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                  <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
-                </svg>
-
-                <span className={""}>Bez laktozy</span>
+              <ul className="list-disc  space-y-2">
+                {ProductInfo.parameters.split(", ").map((item, index) => (
+                    <li key={index} className="font-light text-xl flex items-center space-x-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
+                      </svg>
+                      <span>{item}</span>
+                    </li>
+                ))}
+              </ul>
               </div>
             </div>
           </section>
+
 
           <section>
             <div className="p-4 border-2 border-gray-100 rounded-lg shadow-md">
-              <h1 className="text-2xl text-black">Skład</h1>
-              <div className="font-light text-md flex items-center space-x-4 pl-6">
-                <img
-                    src={"/right-arrow.png"}
-                    alt="Arrow"
-                    className="w-4 h-4 object-contain"
-                />
-                <span>Bez laktozy</span>
-              </div>
-              <div className="font-light text-md flex items-center space-x-4 pl-6">
-                <img
-                    src={"/right-arrow.png"}
-                    alt="Arrow"
-                    className="w-4 h-4 object-contain"
-                />
-                <span>Bez laktozy</span>
-              </div>
-
+              <h1 className="text-2xl text-black mb-2">Skład</h1>
+              <ul className="list-disc pl-8 space-y-2">
+                {ProductInfo.composition.split(", ").map((item, index) => (
+                    <li key={index} className="font-light text-xl flex items-center space-x-4">
+                      <img src={"/right-arrow.png"} alt="Arrow" className="w-4 h-4 object-contain" />
+                      <span>{item}</span>
+                    </li>
+                ))}
+              </ul>
             </div>
           </section>
+
         </div>
 
         <div className="p-4 border-2 border-gray-100 rounded-lg shadow-md md:col-span-4">
